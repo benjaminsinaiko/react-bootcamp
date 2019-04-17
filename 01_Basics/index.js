62ca64085f8d2fb69e245f5a4dc112b28971c6c4
benjaminsinaiko <ben.jamin@outlook.com>
2019-04-17 13:13:31 -0500

    Update folder structure for first two sections

---
+++ 01_Basics/index.js
@@ -0,0 +1,17 @@
+/* eslint-disable react/jsx-no-undef */
+/* eslint-disable react/react-in-jsx-scope */
+/* eslint-disable no-undef */
+/* eslint-disable react/prefer-stateless-function */
+class Hello extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello!</h1>
+        <h1>Hello!</h1>
+        <h1>Hello!</h1>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Hello />, document.getElementById('root'));
