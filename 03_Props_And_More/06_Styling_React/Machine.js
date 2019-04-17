43f24f1c31245d9518126d0f470c1cb791d1ebbe
benjaminsinaiko <ben.jamin@outlook.com>
2019-04-17 17:22:36 -0500

    Add react styling section

---
+++ 03_Props_And_More/06_Styling_React/Machine.js
@@ -0,0 +1,23 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/react-in-jsx-scope */
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prefer-stateless-function */
+class Machine extends React.Component {
+  render() {
+    const { s1, s2, s3 } = this.props;
+    const winner = s1 === s2 && s2 === s3;
+    return (
+      <div className="Machine">
+        <p style={{ fontSize: '50px', backgroundColor: 'purple' }}>
+          {s1} {s2}
+          {s3}{' '}
+        </p>
+        <p className={winner ? 'Machine-winner' : 'Machine-loser'}>
+          {winner ? 'You win!' : 'You lose...'}
+        </p>
+      </div>
+    );
+  }
+}
