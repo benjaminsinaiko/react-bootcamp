3a531f2aad3a319cbef23ecb71f8d3d40f2aa9af
benjaminsinaiko <ben.jamin@outlook.com>
2019-04-24 00:42:11 -0500

    Add update existing state example

--- 06_React_State/state-demo/src/App.js
+++ 06_React_State/state-demo/src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react'
+import './App.css'
+
 import Game from './stateIntro/Game'
 import Demo from './stateIntro/Demo'
 import Button from './stateIntro/Button'
 import BrokenClick from './stateIntro/BrokenClick'
 import Rando from './stateIntro/Rando'
 import Clicker from './stateIntro/Clicker'
-import './App.css'
 
 import RollDice from './diceExercise/RollDice'
+
+import ScoreKeeper from './state-patterns/ScoreKeeper'
 
 class App extends Component {
   render() {
@@ -20,7 +23,9 @@ class App extends Component {
         <Rando maxNum={10} />
         <Clicker /> */}
 
-        <RollDice face="five" />
+        {/* <RollDice face="five" /> */}
+
+        <ScoreKeeper />
       </div>
     )
   }
