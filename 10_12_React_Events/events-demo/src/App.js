f046a0dfa6e17116a2402bc83686c5ac1d21dc71
benjaminsinaiko <ben.jamin@outlook.com>
2019-04-25 00:42:41 -0500

    Add events demo folder, buttonlist exercise

---
+++ 10_12_React_Events/events-demo/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+
+import ButtonList from './buttonList/ButtonList'
+
+function App() {
+  return (
+    <div className="App">
+      <ButtonList />
+    </div>
+  )
+}
+
+export default App
