2c3594b28e01cec2aa84c7e252669f61870228b6
benjaminsinaiko <ben.jamin@outlook.com>
2019-04-26 01:21:10 -0500

    Finish lightsOut exercise

--- 10_12_React_Events/events-demo/src/lightsOut/Board.js
+++ 10_12_React_Events/events-demo/src/lightsOut/Board.js
@@ -2,58 +2,38 @@ import React, { Component } from 'react'
 import Cell from './Cell'
 import './Board.css'
 
-/** Game board of Lights out.
- *
- * Properties:
- *
- * - nrows: number of rows of board
- * - ncols: number of cols of board
- * - chanceLightStartsOn: float, chance any cell is lit at start of game
- *
- * State:
- *
- * - hasWon: boolean, true when board is all off
- * - board: array-of-arrays of true/false
- *
- *    For this board:
- *       .  .  .
- *       O  O  .     (where . is off, and O is on)
- *       .  .  .
- *
- *    This would be: [[f, f, f], [t, t, f], [f, f, f]]
- *
- *  This should render an HTML table of individual <Cell /> components.
- *
- *  This doesn't handle any clicks --- clicks are on individual cells
- *
- **/
-
 class Board extends Component {
   static defaultProps = {
     nRows: 5,
-    nCols: 5
+    nCols: 5,
+    chanceLightStartsOn: 0.25
   }
   constructor(props) {
     super(props)
-
-    // TODO: set initial state
     this.state = {
       hasWon: false,
-      board: []
+      board: this.createBoard()
     }
   }
 
   /** create a board nrows high/ncols wide, each cell randomly lit or unlit */
 
-  createBoard() {
+  createBoard = () => {
     let board = []
-    // TODO: create array-of-arrays of true/false values
+    for (let y = 0; y < this.props.nRows; y++) {
+      let row = []
+      for (let x = 0; x < this.props.nCols; x++) {
+        row.push(Math.random() < this.props.chanceLightStartsOn)
+      }
+      board.push(row)
+    }
     return board
   }
 
   /** handle changing a cell: update board & determine if winner */
 
-  flipCellsAround(coord) {
+  flipCellsAround = coord => {
+    console.log('FLIPPING', coord)
     let { nCols, nRows } = this.props
     let board = this.state.board
     let [y, x] = coord.split('-').map(Number)
@@ -66,34 +46,60 @@ class Board extends Component {
       }
     }
 
-    // TODO: flip this cell and the cells around it
+    flipCell(y, x) // flip clicked cell
+    flipCell(y, x - 1) // flip left cell
+    flipCell(y, x + 1) // flip right cell
+    flipCell(y - 1, x) // flip below cell
+    flipCell(y + 1, x) // flip above cell
 
     // win when every cell is turned off
     // TODO: determine is the game has been won
+    let hasWon = board.every(row => row.every(cell => !cell))
 
-    // this.setState({ board, hasWon })
+    this.setState({ board, hasWon })
   }
 
   /** Render game board or winning message. */
 
   render() {
     // if the game is won, just show a winning msg & render nothing else
+    if (this.state.hasWon) {
+      return (
+        <div className="Board-title">
+          <div className="winner">
+            <span className="neon-orange">YOU</span>
+            <span className="neon-blue">WIN!</span>
+          </div>
+        </div>
+      )
+    }
 
-    // TODO
+    let tblBoard = []
+    for (let y = 0; y < this.props.nRows; y++) {
+      let row = []
+      for (let x = 0; x < this.props.nCols; x++) {
+        let coord = `${y}-${x}`
+        row.push(
+          <Cell
+            key={coord}
+            isLit={this.state.board[y][x]}
+            flipCellsAroundMe={() => this.flipCellsAround(coord)}
+          />
+        )
+      }
+      tblBoard.push(<tr key={y}>{row}</tr>)
+    }
 
-    // make table board
-
-    // TODO
     return (
-      <table className="Board">
-        <tbody>
-          <tr>
-            <Cell isLit={true} />
-            <Cell isLit={false} />
-            <Cell isLit={true} />
-          </tr>
-        </tbody>
-      </table>
+      <div>
+        <div className="Board-title">
+          <div className="neon-orange">Lights</div>
+          <div className="neon-blue">Out</div>
+        </div>
+        <table className="Board">
+          <tbody>{tblBoard}</tbody>
+        </table>
+      </div>
     )
   }
 }
