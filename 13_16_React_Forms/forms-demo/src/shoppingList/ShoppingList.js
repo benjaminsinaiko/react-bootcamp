8624a34c5ad941065d39c54f33df05ef98007e0f
benjaminsinaiko <ben.jamin@outlook.com>
2019-04-29 00:34:29 -0500

    Add uuid package for unique ids

--- 13_16_React_Forms/forms-demo/src/shoppingList/ShoppingList.js
+++ 13_16_React_Forms/forms-demo/src/shoppingList/ShoppingList.js
@@ -1,26 +1,28 @@
 import React, { Component } from 'react'
 import ShoppingListForm from './ShoppingListForm'
+import uuid from 'uuid/v4'
 
 export default class ShoppingList extends Component {
   constructor(props) {
     super(props)
     this.state = {
       items: [
-        { name: 'Milk', qty: '2 gallons' },
-        { name: 'Bread', qty: '2 loaves' }
+        { name: 'Milk', qty: '2 gallons', id: uuid() },
+        { name: 'Bread', qty: '2 loaves', id: uuid() }
       ]
     }
   }
   addItem = item => {
+    let newItem = { ...item, id: uuid() }
     this.setState(prevState => ({
-      items: [...prevState.items, item]
+      items: [...prevState.items, newItem]
     }))
   }
   renderItems() {
     return (
       <ul>
         {this.state.items.map(item => (
-          <li>
+          <li key={item.id}>
             {item.name}: {item.qty}
           </li>
         ))}
