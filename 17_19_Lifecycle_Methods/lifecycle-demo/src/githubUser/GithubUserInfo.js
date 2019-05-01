8509639de92db28a99569da8681250adf938a27a
benjaminsinaiko <ben.jamin@outlook.com>
2019-05-01 15:08:13 -0500

    Add logs for lifecycle methods to see timing

--- 17_19_Lifecycle_Methods/lifecycle-demo/src/githubUser/GithubUserInfo.js
+++ 17_19_Lifecycle_Methods/lifecycle-demo/src/githubUser/GithubUserInfo.js
@@ -7,16 +7,22 @@ class GithubUserInfo extends Component {
     this.state = {
       user: {}
     }
+    console.log('INSIDE CONSTRUCTOR')
   }
   //async version
   async componentDidMount() {
+    console.log('INSIDE COMPONENT DID MOUNT')
     const url = `https://api.github.com/users/${this.props.username}`
     let response = await axios.get(url)
     let user = response.data
     this.setState({ user })
   }
+  componentDidUpdate() {
+    console.log('INSIDE COMPONENT DID UPDATE')
+  }
 
   render() {
+    console.log('INSIDE RENDER')
     const { name, bio, avatar_url, location } = this.state.user
     return (
       <div>
