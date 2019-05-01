5c063891728a511ff560de60ce30764c81057ed4
benjaminsinaiko <ben.jamin@outlook.com>
2019-05-01 12:51:13 -0500

    Add total score, fix bugs

--- 13_16_React_Forms/forms-demo/src/yahtzee/Game.js
+++ 13_16_React_Forms/forms-demo/src/yahtzee/Game.js
@@ -75,10 +75,21 @@ class Game extends Component {
       rollsLeft: NUM_ROLLS,
       locked: Array(NUM_DICE).fill(false)
     }))
-    this.roll()
+    this.animateRoll()
+  }
+
+  displayRollInfo = () => {
+    const messages = [
+      '0 Rolls Left',
+      '1 Roll Left',
+      '2 Rolls Left',
+      'Starting Roll'
+    ]
+    return messages[this.state.rollsLeft]
   }
 
   render() {
+    const { dice, rolling, locked, rollsLeft, scores } = this.state
     return (
       <div className="Game">
         <header className="Game-header">
@@ -86,25 +97,23 @@ class Game extends Component {
 
           <section className="Game-dice-section">
             <Dice
-              dice={this.state.dice}
-              rolling={this.state.rolling}
-              locked={this.state.locked}
+              dice={dice}
+              rolling={rolling}
+              locked={locked}
               handleClick={this.toggleLocked}
-              disabled={this.state.rollsLeft === 0}
+              disabled={rollsLeft === 0}
             />
             <div className="Game-button-wrapper">
               <button
                 className="Game-reroll"
-                disabled={
-                  this.state.locked.every(x => x) || this.state.rollsLeft === 0
-                }
+                disabled={locked.every(x => x) || rollsLeft === 0 || rolling}
                 onClick={this.animateRoll}>
-                {this.state.rollsLeft} Rerolls Left
+                {this.displayRollInfo()}
               </button>
             </div>
           </section>
         </header>
-        <ScoreTable doScore={this.doScore} scores={this.state.scores} />
+        <ScoreTable doScore={this.doScore} scores={scores} />
       </div>
     )
   }
