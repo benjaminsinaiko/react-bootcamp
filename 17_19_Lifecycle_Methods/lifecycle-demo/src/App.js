dc69c7a033f3ffc70dd22daa7c7872344288ca0c
benjaminsinaiko <ben.jamin@outlook.com>
2019-05-01 14:37:01 -0500

    Load ajax data on mount, zenquote example

--- 17_19_Lifecycle_Methods/lifecycle-demo/src/App.js
+++ 17_19_Lifecycle_Methods/lifecycle-demo/src/App.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react'
 import './App.css'
 
 import Timer from './timer/Timer'
-
+import ZenQuotes from './zenQuote/ZenQuote'
 import GithubUserInfo from './githubUser/GithubUserInfo'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Timer />
-
+        {/* <Timer /> */}
+        <ZenQuotes />
         {/* <GithubUserInfo username="Elie" /> */}
       </div>
     )
---
+++ 17_19_Lifecycle_Methods/lifecycle-demo/src/zenQuote/ZenQuote.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+import Loader from './Loader'
+
+class ZenQuote extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      quote: '',
+      isLoaded: false
+    }
+  }
+  componentDidMount() {
+    // load data
+    axios.get('https://api.github.com/zen').then(res => {
+      setTimeout(() => {
+        this.setState({ quote: res.data, isLoaded: true })
+      }, 2000)
+    })
+    // set state with that data
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.isLoaded ? (
+          <div>
+            <h1>Always remember...</h1>
+
+            <p>{this.state.quote}</p>
+          </div>
+        ) : (
+          <Loader />
+        )}
+      </div>
+    )
+  }
+}
+
+export default ZenQuote
