7f684b93d9f6e86dec1c879dba163cf41f27b421
benjaminsinaiko <ben.jamin@outlook.com>
2019-05-02 15:29:42 -0500

    Style joke component, dynamically add vote colors and emojis

--- 17_19_Lifecycle_Methods/lifecycle-demo/src/dadJokes/Joke.js
+++ 17_19_Lifecycle_Methods/lifecycle-demo/src/dadJokes/Joke.js
@@ -2,6 +2,40 @@ import React, { Component } from 'react'
 import './Joke.css'
 
 export default class Joke extends Component {
+  getColor = () => {
+    if (this.props.votes >= 15) {
+      return '#4caf50'
+    } else if (this.props.votes >= 12) {
+      return '#8bc34a'
+    } else if (this.props.votes >= 9) {
+      return '#cddc39'
+    } else if (this.props.votes >= 6) {
+      return '#ffeb3b'
+    } else if (this.props.votes >= 3) {
+      return '#ffc107'
+    } else if (this.props.votes >= 0) {
+      return '#ff9800'
+    } else {
+      return '#f44336'
+    }
+  }
+  getEmoji = () => {
+    if (this.props.votes >= 15) {
+      return 'em em-rolling_on_the_floor_laughing'
+    } else if (this.props.votes >= 12) {
+      return 'em em-laughing'
+    } else if (this.props.votes >= 9) {
+      return 'em em-smiley'
+    } else if (this.props.votes >= 6) {
+      return 'em em-slightly_smiling_face'
+    } else if (this.props.votes >= 3) {
+      return 'em em-neutral_face'
+    } else if (this.props.votes >= 0) {
+      return 'em em-confused'
+    } else {
+      return 'em em-angry'
+    }
+  }
   handleUpvote = () => {
     this.props.upvote(this.props.id, 1)
   }
@@ -14,10 +48,15 @@ export default class Joke extends Component {
       <div className="Joke">
         <div className="Joke-buttons">
           <i className="fas fa-arrow-up" onClick={this.handleUpvote} />
-          <span>{this.props.votes}</span>
+          <span className="Joke-votes" style={{ borderColor: this.getColor() }}>
+            {this.props.votes}
+          </span>
           <i className="fas fa-arrow-down" onClick={this.handleDownvote} />
         </div>
         <div className="Joke-text">{this.props.text}</div>
+        <div className="Joke-smiley">
+          <i className={this.getEmoji()} />
+        </div>
       </div>
     )
   }
