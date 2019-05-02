014705bdb0343e5cbc4ea4da73a2bee04c6c1f4d
benjaminsinaiko <ben.jamin@outlook.com>
2019-05-02 13:28:54 -0500

    Style joke list component

--- 17_19_Lifecycle_Methods/lifecycle-demo/src/dadJokes/JokeList.js
+++ 17_19_Lifecycle_Methods/lifecycle-demo/src/dadJokes/JokeList.js
@@ -32,6 +32,16 @@ export default class JokeList extends Component {
   render() {
     return (
       <div className="JokeList">
+        <div className="JokeList-sidebar">
+          <h1 className="JokeList-title">
+            <span>Dad</span> Jokes
+          </h1>
+          <img
+            src="https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg"
+            alt="face with tears of joy"
+          />
+          <button className="JokeList-getmore">New Jokes</button>
+        </div>
         <h1 className="JokeList-jokes">
           {this.state.jokes.map(j => (
             <div>{j}</div>
