fdaa85d1ee594a39754caa690781564dfd512ab2
benjaminsinaiko <ben.jamin@outlook.com>
2019-05-01 23:34:04 -0500

    Add card component, display cards

---
+++ 17_19_Lifecycle_Methods/lifecycle-demo/src/deckOfCards/Card.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+
+export default class Card extends Component {
+  render() {
+    return <img className="Card" src={this.props.image} alt={this.props.name} />
+  }
+}
