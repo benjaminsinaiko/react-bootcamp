9c97e3b76fc0f762dff5d8cb6af5dad645142288
benjaminsinaiko <ben.jamin@outlook.com>
2019-05-11 13:37:41 -0500

    Add react patterns folder, simple app setup

--- 20_23_React_Router/router-demo/src/App.js
+++ 20_23_React_Router/router-demo/src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
 import { Route, Switch, NavLink } from 'react-router-dom'
-import NavBar from './vendingMachine/NavBar'
-import VendingMachine from './vendingMachine/VendingMachine'
-import Chips from './vendingMachine/Chips'
-import Sardines from './vendingMachine/Sardines'
-import Pop from './vendingMachine/Pop'
+
+// import NavBar from './vendingMachine/NavBar'
+// import VendingMachine from './vendingMachine/VendingMachine'
+// import Chips from './vendingMachine/Chips'
+// import Sardines from './vendingMachine/Sardines'
+// import Pop from './vendingMachine/Pop'
+
+import Food from './routerPatterns/Food'
+import Meal from './routerPatterns/Meal'
+import FoodSearch from './routerPatterns/FoodSearch'
+import Navbar from './routerPatterns/Navbar'
 
 import './App.css'
 
@@ -12,12 +18,28 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <NavBar />
+        {/* <NavBar />
         <Switch>
           <Route exact path="/" component={VendingMachine} />
           <Route exact path="/pop" component={Pop} />
           <Route exact path="/sardines" component={Sardines} />
           <Route exact path="/chips" component={Chips} />
+        </Switch> */}
+
+        <Navbar />
+        <Switch>
+          <Route
+            exact
+            path="/food/:name"
+            render={routerProps => <Food {...routerProps} />}
+          />
+          <Route
+            exact
+            path="/food/:foodName/drink/:drinkName"
+            component={Meal}
+          />
+          <Route exact path="/" component={FoodSearch} />
+          <Route render={() => <h1>ERROR NOT FOUND!!!</h1>} />
         </Switch>
       </div>
     )
