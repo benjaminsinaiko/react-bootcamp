3f99c171fb5ea1db17fe0cdf8d9fff367d789011
benjaminsinaiko <ben.jamin@outlook.com>
2019-05-11 13:43:58 -0500

    Use link to direct from search form

--- 20_23_React_Router/router-demo/src/routerPatterns/FoodSearch.js
+++ 20_23_React_Router/router-demo/src/routerPatterns/FoodSearch.js
@@ -1,36 +1,34 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 class FoodSearch extends Component {
   constructor(props) {
-    super(props);
-    this.state = { query: "" };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleClick = this.handleClick.bind(this);
+    super(props)
+    this.state = { query: '' }
   }
-  handleChange(evt) {
-    this.setState({ query: evt.target.value });
+  handleChange = e => {
+    this.setState({ query: e.target.value })
   }
-  handleClick() {
+  handleClick = () => {
     //Do something
-    alert("SAVED YOUR FOOD TO DB!");
+    alert('SAVED YOUR FOOD TO DB!')
     //redirect somewhere else
-    this.props.history.push(`/food/${this.state.query}`);
+    this.props.history.push(`/food/${this.state.query}`)
   }
   render() {
     return (
       <div>
         <h1>Search For A Food!</h1>
         <input
-          type='text'
-          placeholder='search for a food'
+          type="text"
+          placeholder="search for a food"
           value={this.state.query}
           onChange={this.handleChange}
         />
         <Link to={`/food/${this.state.query}`}>Go!</Link>
         <button onClick={this.handleClick}>Save New Food!</button>
       </div>
-    );
+    )
   }
 }
-export default FoodSearch;
+export default FoodSearch
