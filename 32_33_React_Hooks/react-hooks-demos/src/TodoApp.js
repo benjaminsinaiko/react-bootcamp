fc092ce4874d253637d568913c3b06edf0d746a3
benjaminsinaiko <ben.jamin@outlook.com>
2019-06-11 13:08:55 -0500

    Add TodoList with initialTodos

--- 32_33_React_Hooks/react-hooks-demos/src/TodoApp.js
+++ 32_33_React_Hooks/react-hooks-demos/src/TodoApp.js
@@ -1,7 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Grid from '@material-ui/core/Grid';
+
+import TodoList from './TodoList';
 
 function TodoApp() {
-  return <h1>TODOS</h1>;
+  const initialTodos = [
+    { id: 1, task: 'Walk dog', completed: false },
+    { id: 2, task: 'Take out trash', completed: false },
+    { id: 3, task: 'Order food', completed: false },
+  ];
+  const [todos, setTodos] = useState(initialTodos);
+
+  return (
+    <Paper
+      style={{
+        padding: 0,
+        margin: 0,
+        height: '100vh',
+        backgroundColor: '#fafafa',
+      }}
+    >
+      <AppBar color="primary" position="static" style={{ height: '64px' }}>
+        <Toolbar>
+          <Typography color="inherit">TODOS WITH HOOKS</Typography>
+        </Toolbar>
+      </AppBar>
+      <TodoList todos={todos} />
+    </Paper>
+  );
 }
 
 export default TodoApp;
