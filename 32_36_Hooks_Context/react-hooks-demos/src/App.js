275803f1aad9336b9937bc87a40f6b346da4cbf4
benjaminsinaiko <ben.jamin@outlook.com>
2019-06-13 17:22:12 -0500

    Setup for ContextApp, rename project folder

---
+++ 32_36_Hooks_Context/react-hooks-demos/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import TodoApp from './TodoApp';
+import ContextApp from './contextApp/ContextApp';
+
+function App() {
+  return (
+    // <TodoApp />
+    <ContextApp />
+  );
+}
+
+export default App;
---
+++ 32_36_Hooks_Context/react-hooks-demos/src/contextApp/ContextApp.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ContextApp extends Component {
+  render() {
+    return (
+      <div>
+        <h1>ContextApp</h1>
+      </div>
+    );
+  }
+}
+
+export default ContextApp;
